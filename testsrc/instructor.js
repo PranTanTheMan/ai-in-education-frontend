a7e1317562df801988ba7b6240c8d7fdc626f136
moon <v685958@gmail.com>
2023-10-10 21:50:15 -0700

    changed style.css and student.js files

--- testsrc/instructor.js
+++ testsrc/instructor.js
@@ -5,16 +5,32 @@
 
 function setRubricPoints() {
     const rubrics = [];
+
     for (let i = 1; i <= 4; i++) {
         const rubricDesc = document.getElementById(`rubric${i}Desc`).value;
-        const rubricPoints = parseInt(document.getElementById(`rubric${i}Points`).value);
+        const rubricPoints = parseInt(document.getElementById(`rubric${i}`).value);
         if (rubricDesc && !isNaN(rubricPoints)) {
             rubrics.push({ description: rubricDesc, points: rubricPoints });
         }
     }
+
     const totalScore = parseInt(document.getElementById('totalScore').value);
     localStorage.setItem('rubrics', JSON.stringify(rubrics));
     localStorage.setItem('totalScore', totalScore);
     alert("Rubric Points Set Successfully!");
+
+    
+    updateStudentInterface(rubrics, totalScore);
+}
+
+function updateStudentInterface(rubrics, totalScore) {
+    const rubricsList = document.getElementById('rubricsList');
+    rubricsList.innerHTML = "<h3>Rubrics:</h3>";
+    rubrics.forEach(rubric => {
+        rubricsList.innerHTML += `<p>${rubric.description}: ${rubric.points} points</p>`;
+    });
+
+    const totalScoreDisplay = document.getElementById('totalScoreDisplay');
+    totalScoreDisplay.textContent = totalScore;
 }
 
--- testsrc/student.js
+++ testsrc/student.js
@@ -3,33 +3,41 @@
  **/
 
 document.addEventListener('DOMContentLoaded', () => {
+    const rubricsList = document.getElementById('rubricsList');
+    const totalScoreDisplay = document.getElementById('totalScoreDisplay');
+    const gradingForm = document.getElementById('grading-form');
+    const loadingIcon = document.querySelector('.loading-icon');
+    const resultContainer = document.querySelector('.result-container');
+
     const rubrics = JSON.parse(localStorage.getItem('rubrics')) || [];
     const totalScore = parseInt(localStorage.getItem('totalScore')) || 0;
 
-    const rubricsList = document.getElementById('rubricsList');
     rubricsList.innerHTML = "<h3>Rubrics:</h3>";
     rubrics.forEach(rubric => {
         rubricsList.innerHTML += `<p>${rubric.description}: ${rubric.points} points</p>`;
     });
 
-    const totalScoreDisplay = document.getElementById('totalScoreDisplay');
     totalScoreDisplay.textContent = totalScore;
 
-    document.getElementById('grading-form').addEventListener('submit', async function (event) {
+    gradingForm.addEventListener('submit', async function (event) {
         event.preventDefault();
 
         const fileInput = document.getElementById('file');
         const file = fileInput.files[0];
 
         if (file) {
+            loadingIcon.style.display = 'block';
+
             try {
                 const extractedText = await extractTextFromImage(file);
                 const gptResponse = await sendToGPT(extractedText, rubrics, totalScore);
 
-                //displaying the response
-                const resultContainer = document.querySelector('.result-container');
+                loadingIcon.style.display = 'none';
+
                 resultContainer.innerHTML = `<h2>Grading Result:</h2><p>Score: ${gptResponse.score}</p><p>Feedback: ${gptResponse.feedback}</p>`;
             } catch (error) {
+                loadingIcon.style.display = 'none';
+
                 alert("Error occurred while processing the document. Please try again.");
                 console.error(error);
             }
@@ -37,53 +45,61 @@ document.addEventListener('DOMContentLoaded', () => {
             alert("Please upload a file before grading.");
         }
     });
+
+    async function extractTextFromImage(imageFile) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = async function () {
+                const image = new Image();
+                image.src = reader.result;
+                image.onload = function () {
+                    const canvas = document.createElement('canvas');
+                    const context = canvas.getContext('2d');
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    context.drawImage(image, 0, 0);
+                    Tesseract.recognize(
+                        canvas,
+                        'eng',
+                        { logger: info => console.log(info) }
+                    ).then(({ data: { text } }) => {
+                        resolve(text);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                };
+            };
+            reader.onerror = reject;
+            reader.readAsDataURL(imageFile);
+        });
+    }
+
+    function sendToGPT(text, rubrics, totalScore) {
+        return new Promise((resolve, reject) => {
+            const apiUrl ='' //enter the api endpoint; 
+    
+            const requestData = {
+                text: text,
+                rubrics: rubrics,
+                totalScore: totalScore
+            };
+    
+            fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestData)
+            })
+            .then(response => response.json())
+            .then(data => {
+                const score = data.score;
+                const feedback = data.feedback;
+                resolve({ score, feedback });
+            })
+            .catch(error => {
+                reject(error);
+            });
+        });
+    }
 });
-
-async function extractTextFromImage(imageFile) {
-    return new Promise((resolve, reject) => {
-        // assuming you guys have tesseract.js set up for client-side OCR
-        const Tesseract = window.Tesseract;
-        Tesseract.recognize(
-            imageFile,
-            'eng', 
-            { logger: info => console.log(info) } 
-        ).then(({ data: { text } }) => {
-            resolve(text);
-        }).catch(error => {
-            reject(error);
-        });
-    });
-}
-
-async function sendToGPT(text, rubrics, totalScore) {
-    return new Promise((resolve, reject) => {
-        const apiUrl = //put api endpoint here; 
-
-        // preparing the data here
-        const requestData = {
-            text: text,
-            rubrics: rubrics,
-            totalScore: totalScore
-        };
-
-        // sending the prepared data
-        fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestData)
-        })
-        .then(response => response.json())
-        .then(data => {
-            // handle the response from the GPT API
-            const score = data.score;
-            const feedback = data.feedback;
-            resolve({ score, feedback });
-        })
-        .catch(error => {
-            reject(error);
-        });
-    });
-}
-
